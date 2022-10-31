7efab20bafdca8e9e2ac89f08ad04fd1091bdecf
Izzat <superizzat564@gmail.com>
2022-10-31 18:48:06 +0500

    I finished skills section

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,8 +6,17 @@
     console.log(hamburger);
     hamburger.addEventListener('click', function(){
         menu.classList.add('active');
+        document.body.style.overflow = 'hidden';
     });
     close.addEventListener('click', function(){
         menu.classList.remove('active');
+        document.body.style.overflow = 'scroll';
     });
+
+    const counters = document.querySelectorAll('.skills__ratings-counter'),
+          lines = document.querySelectorAll('.skills__ratings-line span');
+          counters.forEach((item, i) =>{
+            lines[i].style.width = item.innerHTML;
+          });
+
  });
