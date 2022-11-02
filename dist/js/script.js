c377c29a3d6b2f3c8d1afe9dda7609119dfdbc85
Izzat <superizzat564@gmail.com>
2022-11-02 11:40:05 +0500

    Menu and overlay click

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,21 +2,20 @@
     const hamburger = document.querySelector('.hamburger');
     const menu = document.querySelector('.menu');
     const close = document.querySelector('.menu__close');
+    const overlay = document.querySelector('.menu__overlay');
     console.log(menu);
     console.log(hamburger);
     hamburger.addEventListener('click', function(){
         menu.classList.add('active');
         document.body.style.overflow = 'hidden';
+
     });
     close.addEventListener('click', function(){
         menu.classList.remove('active');
         document.body.style.overflow = 'scroll';
     });
-
-    // const counters = document.querySelectorAll('.skills__ratings-counter'),
-    //       lines = document.querySelectorAll('.skills__ratings-line span');
-    //       counters.forEach((item, i) =>{
-    //         lines[i].style.width = item.innerHTML;
-    //       });
-
+    overlay.addEventListener('click', function(){
+        menu.classList.remove('active');
+        document.body.style.overflow = 'scroll';
+    });
  });
--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,20 @@
     const hamburger = document.querySelector('.hamburger');
     const menu = document.querySelector('.menu');
     const close = document.querySelector('.menu__close');
+    const overlay = document.querySelector('.menu__overlay');
     console.log(menu);
     console.log(hamburger);
     hamburger.addEventListener('click', function(){
         menu.classList.add('active');
+        document.body.style.overflow = 'hidden';
+
     });
     close.addEventListener('click', function(){
         menu.classList.remove('active');
+        document.body.style.overflow = 'scroll';
+    });
+    overlay.addEventListener('click', function(){
+        menu.classList.remove('active');
+        document.body.style.overflow = 'scroll';
     });
  });
