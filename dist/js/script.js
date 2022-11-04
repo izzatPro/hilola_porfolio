b67279fc2dfe8f94287ebf293664db95393192fd
Izzat <superizzat564@gmail.com>
2022-11-04 18:31:18 +0500

    Counter skills mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,8 @@
     const menu = document.querySelector('.menu');
     const close = document.querySelector('.menu__close');
     const overlay = document.querySelector('.menu__overlay');
+    const counter = document.querySelectorAll('.skills__ratings-counter');
+    const line = document.querySelectorAll('.skills__ratings-line');
     console.log(menu);
     console.log(hamburger);
     hamburger.addEventListener('click', function(){
@@ -17,5 +19,9 @@
     overlay.addEventListener('click', function(){
         menu.classList.remove('active');
         document.body.style.overflow = 'scroll';
+    });
+    counter.forEach((item,i) =>{
+        line[i].style.width = item.innerHTML;
+        console.log(item.innerHTML);
     });
  });
